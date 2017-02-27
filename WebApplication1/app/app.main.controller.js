25b9b2825b07979ed55deab9334df64454bccb37
Rodolfo Viant <rviant@bulletline.com>
2017-02-27 16:54:18 -0500

    First Commit

---
+++ WebApplication1/app/app.main.controller.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Main', Main);
+
+    Main.$inject = [];
+
+    function Main() {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'Angular Example for Freddy';
+        vm.food = '';
+
+        init();
+
+        function init() {
+            console.log("Controller started");
+
+        }
+    }
+})();
